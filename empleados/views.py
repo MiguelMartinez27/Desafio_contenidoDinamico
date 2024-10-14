760d724a97ecc4b41f7f9ef0ff3bb65b52837427
Miguel Martínez <miguel.martinez.fue@gmail.com>
2024-10-14 19:50:49 -0300

    contenido dinamico agregado

--- empleados/views.py
+++ empleados/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def vista_empleados(request):
-    return render(request, "empleados/vista.html")
+    listado = ["Carlos", "Juana", "Florencia", "Roberto", "Jose", "Manuela"]
+    context = {"categories": listado}
+    return render(request, "empleados/vista.html", context)
